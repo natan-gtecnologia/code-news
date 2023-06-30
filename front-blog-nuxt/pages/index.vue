8a7f67e328d71f32fc90cee8ed6e71a064e247f5
natan.gtecnologia <natanfalconi.gtecnologia@gmail.com>
2023-06-30 15:51:53 -0300

    refatoring

--- front-blog-nuxt/pages/index.vue
+++ front-blog-nuxt/pages/index.vue
@@ -34,14 +34,14 @@ export default {
 
 <template>
   <div class="w-1/2 mx-auto my-20">
-    <h1 class="text-5xl font-semibold text-indigo-400">{{ titleBlog }}</h1>
+    <h1 class="text-5xl font-semibold text-indigo-400 font-mono">{{ titleBlog }}</h1>
 
     <article class="my-20" v-for="(article, index) in listArticle" :key="index">
-      <h2 class="text-4xl font-semibold mb-5 text-indigo-500 ">{{ article.titulo }}</h2>
-      <p class="mb-5 text-gray-200">{{ `${article.descricao.substr(0, 500)}` + " ..." }}</p>
-      <p class="text-blue-400 flex justify-end">{{ article.autor.nome }}</p>
+      <h2 class="text-4xl font-semibold mb-5 font-mono text-indigo-500 ">{{ article.titulo }}</h2>
+      <p class="mb-5 text-gray-200 font-thin">{{ `${article.descricao.substr(0, 300)}` + " ..." }}</p>
+      <p class="text-blue-400 flex justify-end font-mono">{{ article.autor.nome }}</p>
 
-      <button class="bg-red-600 rounded-full w-28 h-7 text-gray-200" @click="handleClick(article.id)">
+      <button class="bg-red-600 rounded-full w-28 h-7 text-gray-200 font-thin" @click="handleClick(article.id)">
         Veja mais
       </button>
      
--- front-blog-nuxt/pages/news/[id].vue
+++ front-blog-nuxt/pages/news/[id].vue
@@ -1,44 +1,46 @@
 <template>
   <div class="w-1/2 mx-auto my-20">
     <div class="mt-20 mb-10">
-      <h2 class="text-5xl font-semibold mb-5 text-indigo-500">{{ article.titulo }}</h2>
-      <p class="mb-5 text-gray-200">{{ article.descricao }}</p>
-      
-      <div class="flex items-end flex-col">
-        <p class="text-blue-400 ">{{ article.autor?.nome }}</p>
-        <span class="text-gray-200 text-sm"> {{ article.autor?.formacao }}</span>
-        <span class="text-gray-200 text-sm"> {{ article.autor?.nivel }}</span>
+      <h2 class="text-5xl font-semibold mb-5 text-indigo-500 font-mono">
+        {{ article?.titulo }}
+      </h2>
+      <p class="mb-5 text-gray-200 font-thin">{{ article?.descricao }}</p>
+
+      <div class="flex items-end flex-col font-mono">
+        <p class="text-blue-400">{{ article?.autor?.nome }}</p>
+        <span class="text-gray-200 text-sm">
+          {{ article?.autor?.formacao }}</span
+        >
+        <span class="text-gray-200 text-sm"> {{ article?.autor?.nivel }}</span>
       </div>
     </div>
 
-    <button class="bg-red-600 rounded-full w-28 h-7 text-gray-200">
-      <NuxtLink to="/"> Voltar </NuxtLink>
-    </button>
+    <NuxtLink to="/">
+        <button class="bg-red-600 rounded-full w-28 h-7 text-gray-200 font-thin">
+          Voltar 
+        </button>
+      </NuxtLink>
   </div>
-
 </template>
 
-<script>
+<script setup>
 import api from "../../services/api";
-export default {
-  data() {
-    return {
-      article: {},
-    };
-  },
-  methods: {
-    async getArticle() {
-      try {
-        const {data} = await api.get(`noticias/${this.$route.params.id}`);
-        this.article = data;
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
-  mounted() {
-    this.getArticle();
-  },
-};
+const article = ref({});
+const route = useRoute();
+
+async function getArticle() {
+  try {
+    const { data } = await api.get(`noticias/${route.params.id}`);
+    article.value = data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+onMounted(() => {
+  getArticle();
+});
 </script>
