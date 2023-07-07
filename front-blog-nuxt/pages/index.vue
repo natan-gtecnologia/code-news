5d3a54f140be4a65999305d7538b8aa66ae2ccb9
natan.gtecnologia <natanfalconi.gtecnologia@gmail.com>
2023-07-07 11:59:21 -0300

    refatoring

--- front-blog-nuxt/pages/index.vue
+++ front-blog-nuxt/pages/index.vue
@@ -1,6 +1,6 @@
 <script>
 import useApi from "../services/UseApi";
-import { defineComponent, onMounted} from 'vue'
+import { defineComponent, onMounted } from 'vue'
 import { useRouter } from '#vue-router'
 
 export default defineComponent({
@@ -47,15 +47,18 @@ export default defineComponent({
       <h2 class="text-4xl font-semibold mb-5 font-mono text-indigo-500 ">{{ article.title }}</h2>
       <p class="mb-5 text-gray-200 font-thin" v-html="article.description.substr(0, 300) + ' ...'"></p>
 
-      <div class="flex sm:justify-end gap-3 mb-4 justify-start ">
-        <p class="text-blue-400 font-mono">{{ article.author.name }} |</p>
-        <p class="text-blue-400 font-mono">{{ new Date(article.created_at).toLocaleDateString() }}</p>
+      <div class="flex sm:justify-end gap-3 mb-4 justify-start">
+        <a href="https://linkedin.com.br/in/natan-falconi"
+          class="transition duration-150 border-b-4 border-transparent hover:border-blue-500" target="_blank">
+          <p class="text-blue-400 font-mono p-2 rounded-md">{{ article?.author?.name }} | {{ new
+            Date(article?.created_at).toLocaleDateString() }}</p>
+        </a>
       </div>
 
-      <button class="bg-red-600 rounded-full w-32 h-8 text-gray-200 font-mono font-bold uppercase" @click="handleClick(article.id)">
+      <button class="block bg-red-600 rounded-full w-32 h-8 text-gray-200 font-mono font-bold uppercase"
+        @click="handleClick(article.id)">
         Veja mais >
       </button>
-     
+
     </article>
-  </div>
-</template>
+  </div></template>
